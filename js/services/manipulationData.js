05b34964e953b681719d746860e39ef3c394252b
bhsenna <bhsenna@gmail.com>
2025-04-10 20:15:41 -0300

    Corrigido erro de teste async

--- js/services/manipulationData.js
+++ js/services/manipulationData.js
@@ -1,23 +1,25 @@
 const postUser = require('./api');
 const validateInputs = require('./util');
 
-async function inserirUsuario(data) {
+function inserirUsuario(data) {
 
-    let test = validateInputs(data);
+    return new Promise((retorno) => {
+        let test = validateInputs(data);
 
-    if(!test) {
-        await postUser(data)
-        .then(res => {
-            // console.log(res);
-            // alert(`Inserted user: ${res}`);
-            return true;
-        }).catch(err => {
-            return false;
-        })
-    } else {
-        alert(`Preencha o campo ${test}`);
-        return false;
-    }
+        if(!test) {
+            postUser(data)
+            .then(res => {
+                // console.log(res);
+                // alert(`Inserted user: ${res}`);
+                retorno(true);
+            }).catch(err => {
+                retorno(false);
+            })
+        } else {
+            // alert(`Preencha o campo ${test}`);
+            retorno(false);
+        }
+    })    
 }
 
 module.exports = inserirUsuario;
