1a88c340e16aeb2f067b409d93c72941196e3900
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-04-16 12:46:47 -0300

    Criado manipulationsEvents.js para simular as funções da tela de Login

---
+++ src/utils/manipulationsEvents.js
@@ -0,0 +1,33 @@
+import { formatEmail, hasEmailRegistered, isValidEmail } from '@/utils/validations';
+import { getUsers } from '@/services/authService';
+import { checkPassword } from '@/utils/criptopass';
+
+export async function Login(formData) {
+    try {
+        const credentials = formData;
+
+        if (!formData.email || !formData.password) throw new Error('Email e Senha obrigatórios!');
+
+        const formatCredentials = {
+            email: formatEmail(credentials.email),
+            password: credentials.password
+        }
+
+        if (!isValidEmail(formatCredentials.email)) throw new Error('Insira um email válido!');
+        
+        const listUsers = await getUsers();
+        const user = listUsers.find(u => u.email === formatCredentials.email);
+        
+        if (!user) {
+            throw new Error('Email não cadastrado!');
+        }
+
+        if (! await checkPassword(formatCredentials.password , user.password)) {
+            throw new Error('Senha incorreta!');
+        }
+        
+        return (`Bem-vindo de volta, ${user.name}!`);
+    } catch (err) {
+        return (err.message || 'Falha no login. Tente novamente.');
+    }
+}
