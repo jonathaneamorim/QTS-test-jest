c561b4bb385da85c70bba44124c95b6c8d6b7d49
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-04-16 12:49:01 -0300

    Adicionado casos de teste 01-05 ao utils.test.js (criado usuario teste@gmail.com com senha Teste@1234)

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,15 +1,66 @@
-const utils = require('../src/utils/validations');
+const utils = require('@/utils/manipulationsEvents');
 
-test('Teste de email inválido', () => {
-  expect(utils.isValidEmail('a@a')).toBe(false);
+test('CT01 - Login com credenciais válidas', async () => {
+  expect(await utils.Login({
+    email: 'teste@gmail.com',
+    password: 'Teste@1234'
+  })).toContain('Bem-vindo de volta,');
 });
 
-test('Teste de senha válido', () => {
-  expect(utils.isStrongPassword('AadaA@#324')).toBe(true);
+test('CT02 - Login com senha incorreta', async () => {
+  expect(await utils.Login({
+    email: 'teste@gmail.com',
+    password: 'senhaErrada'
+  })).toBe('Senha incorreta!');
 });
 
+test('CT03 - Login com e-mail não cadastrado', async () => {
+  expect(await utils.Login({
+    email: 'emailerrado@gmail.com',
+    password: 'Teste@1234'
+  })).toBe('Email não cadastrado!');
+});
 
+test('CT04 - Campos obrigatórios em branco', async () => {
+  expect(await utils.Login({
+    email: '',
+    password: ''
+  })).toBe('Email e Senha obrigatórios!');
 
-// test('Teste de email válido', () => {
-//   expect(validations.isValidEmail('a@a')).toBe(true);
-// });
+  expect(await utils.Login({
+    email: 'teste@gmail.com',
+    password: ''
+  })).toBe('Email e Senha obrigatórios!');
+
+  expect(await utils.Login({
+    email: '',
+    password: 'Teste@1234'
+  })).toBe('Email e Senha obrigatórios!');
+
+  expect(await utils.Login({
+    email: null,
+    password: 'Teste@1234'
+  })).toBe('Email e Senha obrigatórios!');
+
+  expect(await utils.Login({
+    email: 'teste@gmail.com',
+    password: null
+  })).toBe('Email e Senha obrigatórios!');
+
+  expect(await utils.Login({
+    email: null,
+    password: null
+  })).toBe('Email e Senha obrigatórios!');
+
+  expect(await utils.Login({
+    email: undefined,
+    password: undefined
+  })).toBe('Email e Senha obrigatórios!');
+});
+
+test('CT05 - Formato inválido de e-mail', async () => {
+  expect(await utils.Login({
+    email: 'formatoInvalido',
+    password: 'Teste@1234'
+  })).toBe('Insira um email válido!');
+});
