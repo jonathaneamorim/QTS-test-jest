088052d0ab0d08e07f31f0e41210737538730510
bhsenna <bernardo.h24@aluno.ifsc.edu>
2025-04-16 14:00:27 -0300

    Alterado parametros de resultado teste para serem mais precisos

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,63 +4,63 @@ test('CT01 - Login com credenciais válidas', async () => {
   await expect(utils.login({
     email: 'teste@gmail.com',
     password: 'Teste@1234'
-  })).resolves.not.toThrow();
+  })).resolves.toBeDefined();
 });
 
 test('CT02 - Login com senha incorreta', async () => {
   await expect(utils.login({
     email: 'teste@gmail.com',
     password: 'senhaErrada'
-  })).rejects.toThrow('Senha incorreta!');
+  })).rejects.toThrow(new Error('Senha incorreta!'));
 });
 
 test('CT03 - Login com e-mail não cadastrado', async () => {
   await expect(utils.login({
     email: 'emailerrado@gmail.com',
     password: 'Teste@1234'
-  })).rejects.toThrow('Email não cadastrado!');
+  })).rejects.toThrow(new Error('Email não cadastrado!'));
 });
 
 test('CT04 - Campos obrigatórios em branco', async () => {
   await expect(utils.login({
     email: '',
     password: ''
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: 'teste@gmail.com',
     password: ''
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: '',
     password: 'Teste@1234'
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: null,
     password: 'Teste@1234'
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: 'teste@gmail.com',
     password: null
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: null,
     password: null
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 
   await expect(utils.login({
     email: undefined,
     password: undefined
-  })).rejects.toThrow('Email e Senha obrigatórios!');
+  })).rejects.toThrow(new Error('Email e Senha obrigatórios!'));
 });
 
 test('CT05 - Formato inválido de e-mail', async () => {
   await expect(utils.login({
     email: 'formatoInvalido',
     password: 'Teste@1234'
-  })).rejects.toThrow('Insira um email válido!');
+  })).rejects.toThrow(new Error('Insira um email válido!'));
 });
